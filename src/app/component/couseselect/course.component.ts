b01789272524457df13457e15eb27bc1b1d6809e
mren1112 <awongod@gmail.com>
2020-09-17 19:21:15 +0700

    add edit

--- src/app/component/couseselect/course.component.ts
+++ src/app/component/couseselect/course.component.ts
@@ -507,6 +507,7 @@ export class CourseComponent implements OnInit {
 
 
   addData(obj: any, index: any): void {
+    this.selectedSection[index] = '';
     //  this.todoSelectCourse.splice(index,1)
     //sessionStorage.setItem("todoSelectCourse", JSON.stringify(this.todoSelectCourse));
     console.log('index = ' + index);
@@ -559,6 +560,7 @@ export class CourseComponent implements OnInit {
       if (arr.courseno == courseno) {
         arr.examdate = tmpdatetoStr;
         arr.section = '';
+
         this.checkConfirm();
         //arr.secstatus = !arr.secstatus;
       }
